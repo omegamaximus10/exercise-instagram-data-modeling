e663bf0dfcee9f4f286c4ad1cc5bdd0ba6e7f556
omegamaximus10 <bryan_2904@hotmail.com>
2021-08-13 16:25:49 +0000

    ejercicio finalizado

--- src/models.py
+++ src/models.py
@@ -7,32 +7,30 @@ from sqlalchemy import create_engine
 from eralchemy import render_er
 
 Base = declarative_base()
-
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    username = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column (Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+class Media(Base):
+    __tablename__ ='media'
+    id = Column (Integer, primary_key=True)
+    type = Column (Integer, primary_key=True)
+    url = Column (String (250), nullable =False)
+    post_id = Column(Integer, ForeignKey('post.id'))
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column (Integer, primary_key= True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
 
-    def to_dict(self):
-        return {}
 
-## Draw from SQLAlchemy base
-try:
-    result = render_er(Base, 'diagram.png')
-    print("Success! Check the diagram.png file")
-except Exception as e:
-    print("There was a problem genering the diagram")
-    raise e
+result = render_er(Base, 'diagram.png')
+   
